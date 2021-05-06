4de058de6c726a324ef32dc25560cc4ac6ab120a
Ewerton W. Silva <Ewerton.W.Silva.EXT@thomsonreuters.com>
2021-05-05 23:21:37 -0300

    Add project files.

---
+++ src/NerdStore.Core/DomainObjects/Entity.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NerdStore.Core.DomainObjects
+{
+    public abstract class Entity
+    {
+        public Guid Id { get; set; }
+
+        protected Entity()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public override bool Equals(object obj)
+        {
+            var compareTo = obj as Entity;
+
+            if (ReferenceEquals(this, compareTo)) return true;
+            if (ReferenceEquals(null, compareTo)) return false;
+
+            return Id.Equals(compareTo.Id);
+        }
+
+        public static bool operator ==(Entity entityA, Entity entityB)
+        {
+            var entityAIsNull = ReferenceEquals(entityA, null);
+            var entityBIsNull = ReferenceEquals(entityB, null);
+
+            if (entityAIsNull && entityBIsNull) return true;
+            if (entityAIsNull || entityBIsNull) return false;
+
+            return entityA.Equals(entityB);
+        }
+
+        public static bool operator !=(Entity entityA, Entity entityB)
+        {
+            return !(entityA == entityB);
+        }
+
+        public override int GetHashCode()
+        {
+            return (GetType().GetHashCode() * 268) + Id.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return $"{GetType().Name} [Id={Id}]";
+        }
+    }
+}
---
+++ src/NerdStore.Core/DomainObjects/IAggregateRoot.cs
@@ -0,0 +1,5 @@
+﻿namespace NerdStore.Core.DomainObjects
+{
+    //Interface de marcacao
+    public interface IAggregateRoot {}
+}
