4d88838d651c42f945c467fd62b5fec7cca93ea0
Ewerton W. Silva <Ewerton.W.Silva.EXT@thomsonreuters.com>
2021-05-09 15:20:18 -0300

    Estoque service

---
+++ src/NerdStore.Catalogo.Domain/EstoqueService.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NerdStore.Catalogo.Domain
+{
+    //Serviço de domínio: ele é cross aggregate
+    public class EstoqueService : IEstoqueService, IDisposable
+    {
+        private readonly IProdutoRepository _produtoRepository;
+
+        public EstoqueService(IProdutoRepository produtoRepository)
+        {
+            _produtoRepository = produtoRepository;
+        }
+
+        public async Task<bool> DebitarEstoque(Guid produtoId, int quantidade)
+        {
+            var produto = await _produtoRepository.ObterPorId(produtoId);
+
+            if (produto == null)
+            {
+                return false; //podemos tbm lançar uma exception
+            }
+
+            if (!produto.PossuiEstoque(quantidade))
+            {
+                return false;
+            }
+
+            produto.DebitarEstoque(quantidade);
+
+            return await AtualizarProduto(produto);
+        }               
+
+        public async Task<bool> ReporEstoque(Guid produtoId, int quantidade)
+        {
+            var produto = await _produtoRepository.ObterPorId(produtoId);
+
+            if (produto == null)
+            {
+                return false; //podemos tbm lançar uma exception
+            }
+
+            produto.ReporEstoque(quantidade);
+
+            return await AtualizarProduto(produto);
+        }
+
+        private async Task<bool> AtualizarProduto(Produto produto)
+        {
+            _produtoRepository.Atualizar(produto);
+            return await _produtoRepository.UnitOfWork.Commit();
+        }
+        public void Dispose()
+        {
+            _produtoRepository.Dispose();
+        }
+    }
+}
