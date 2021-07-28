f15c501983a393ed54be8b9e5d3f93e003be6b5d
Ewerton Willams <ewertonws@gmail.com>
2021-07-27 21:46:47 -0300

    Dominio de vendas em andamento

---
+++ src/NerdStore.Vendas.Domain/ItemPedido.cs
@@ -0,0 +1,53 @@
+﻿using NerdStore.Core.DomainObjects;
+using System;
+
+namespace NerdStore.Vendas.Domain
+{
+    public class ItemPedido : Entity
+    {
+        public Guid PedidoId { get; private set; }
+        public Guid ProdutoId { get; private set; }
+        public string ProdutoNome { get; private set; }
+        public int Quantidade { get; private set; }
+        public decimal ValorUnitario { get; private set; }
+
+        //EF Rel.
+        public Pedido Pedido { get; private set; }
+
+        public ItemPedido(
+            Guid produtoId,
+            string produtoNome,
+            int quantidade,
+            int valorUnitario)
+        {
+            ProdutoId = produtoId;
+            ProdutoNome = produtoNome;
+            Quantidade = quantidade;
+            ValorUnitario = valorUnitario;
+        }
+
+        protected ItemPedido()
+        {
+        }
+
+        internal void AssociarPedido(Guid pedidoId)
+        {
+            PedidoId = pedidoId;
+        }
+
+        public decimal CalcularValor()
+        {
+            return Quantidade * ValorUnitario;
+        }
+
+        internal void AumentarEstoque(int unidades)
+        {
+            Quantidade += unidades;
+        }
+
+        internal void AtualizarQuantidadeEstoque(int quantidade)
+        {
+            Quantidade = quantidade;
+        }
+    }
+}
---
+++ src/NerdStore.Vendas.Domain/Pedido.cs
@@ -0,0 +1,76 @@
+﻿using NerdStore.Core.DomainObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NerdStore.Vendas.Domain
+{
+    public class Pedido : Entity, IAggregateRoot
+    {
+        public int Codigo { get; private set; }
+        public Guid ClientId { get; private set; }
+        public Guid? VoucherId { get; private set; }
+        public bool VoucherUtilizado { get; private set; }
+        public decimal Desconto { get; private set; }
+        public decimal ValorTotal { get; private set; }
+        public DateTime DataCadastro { get; private set; }
+        public PedidoStatus PedidoStatus { get; private set; }
+
+        private readonly List<ItemPedido> _itensPedido;
+        public IReadOnlyCollection<ItemPedido> ItensPedido => _itensPedido;
+        //EF Relationship
+        public virtual Voucher Voucher { get; private set; }
+
+        public Pedido(Guid clientId, bool voucherUtilizado, decimal desconto, decimal valorTotal)
+        {
+            ClientId = clientId;
+            VoucherUtilizado = voucherUtilizado;
+            Desconto = desconto;
+            ValorTotal = valorTotal;
+            _itensPedido = new List<ItemPedido>();
+        }
+
+        public Pedido()
+        {
+            _itensPedido = new List<ItemPedido>();
+        }
+
+        public void AplicarVoucher(Voucher voucher)
+        {
+            Voucher = voucher;
+            VoucherUtilizado = true;
+            CalcularValorPedido();
+        }
+
+        public void CalcularValorPedido()
+        {
+            ValorTotal = ItensPedido.Sum(p => p.CalcularValor());
+            CalcularValorTotalDeconto();
+        }
+
+        public void CalcularValorTotalDeconto()
+        {
+            if (!VoucherUtilizado) return;
+
+            decimal desconto = 0;
+            var valor = ValorTotal;
+
+            if (Voucher.TipoDescontoVouncher == TipoDescontoVouncher.Porcentagem)
+            {
+                if (Voucher.Percentual.HasValue)
+                {
+                    desconto = (valor * Voucher.Percentual.Value) / 100;
+                    valor -= desconto;
+                }
+            }
+            else if (Voucher.ValorDesconto.HasValue)
+            {
+                desconto = Voucher.ValorDesconto.Value;
+                valor -= desconto;
+            }
+
+            ValorTotal = valor < 0 ? 0 : valor;
+            Desconto = desconto;
+        }
+    }
+}
